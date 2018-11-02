4d9d6eb897a9c2a86690ffd05b118eb269ed9899
Sergio <harrizmat@mail.ru>
2018-11-02 14:54:34 +0700

    Залил исходники кода самой первой версии.
    Сделал форматирование кода в Arduino IDE
    Просьба Вам тоже пока что делать пул реквесты в ветку  v2 с кодом, прежде исправленным и отформатированным в Arduino IDE

--- Smart_House/Smart_House.ino
+++ Smart_House/Smart_House.ino
@@ -1,26 +1,47 @@
-#include "DHT.h"
+
+
+//          РИЧАРД   !
+// допишите ниже в комментарии Алгоритма работы
+//свой алгоритм нового кода.
+//мне это поможет и Вам в будущем.
+// эти строки можете удалить, как только алгоритм допишите
+//и пустые строки тоже
 
 
 
-// *************************************** Начало настроек *****************************
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
- * Алгоритм работы 
+   Алгоритм работы
     1)  Короткое нажатие на любую кнопку включает\выключает соответствующий свет
     2) Длительное нажатие на любую кнопку выключает все группы света
     3) Длительное (3 сек) одновременное нажатие на 1 и 4 кнопки включает \ отключает автоматический режим работы клапана обогрева
     о чем свидетельствует плавное включение, выключение сигнальных светодиодов
     4) нажатие на 2 и 3 кнопки на пол секунды включает \выключает клапан вручную в том случае, если авто режим работы отключен
 */
+#include "DHT.h"
+
 // к какому пину подключен сигнальный контакт датчика DHT
-#define DHTPIN 2     
+#define DHTPIN 2
 // максимальная температура на которой клапан будет отключаться в режиме авто
 //важно ее указывать с точкой и двумя нулями после
 #define MAXTEMPERATURE 26.00
 
 // тип датчика температуры, раскомментировать нужную из трех строк
-#define DHTTYPE DHT11   
-//#define DHTTYPE DHT22   
-//#define DHTTYPE DHT21   
+#define DHTTYPE DHT11
+//#define DHTTYPE DHT22
+//#define DHTTYPE DHT21
 
 // Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
 
@@ -31,8 +52,8 @@
 #define BUTTON2 A2 // кнопка включения лампы №2
 #define BUTTON3 A3 // кнопка включения лампы №3
 #define BUTTON4 A4 // кнопка включения светодиодной подсветки
-// в зависимости от типа кнопки, если это обычная механическая кнопка, 
-//подключенная к GND, то устанавливаем ACTIVE 0 
+// в зависимости от типа кнопки, если это обычная механическая кнопка,
+//подключенная к GND, то устанавливаем ACTIVE 0
 // если электронная кнопка, возможно ACTIVE 1
 #define ACTIVE 0
 
@@ -46,7 +67,7 @@
 // чтобы убедиться что эти параметры установлены правильно, при первом включении после перепрошивки
 // все лампы должны быть выключены, а все подсветки включателей - светиться
 #define RELAY_ON 0
-#define RELAY_OFF 1 
+#define RELAY_OFF 1
 
 // пин клапана воды
 #define KLAPAN 8
@@ -74,9 +95,9 @@ bool stateklapan = 0; // состояние клапан открыто \ зак
 bool klapanmode = 0; // состояние режима работы клапана. 0 - ручной режим, 1 - авто режим от датчика температуры
 
 
-byte autoBut = 0; // автомат обработки всех 4  кнопок 
+byte autoBut = 0; // автомат обработки всех 4  кнопок
 byte autoButprev  = 0; // для отладки
-unsigned long ms =0, butMs = 0;
+unsigned long ms = 0, butMs = 0;
 // счетчик верных нажатий ( отпусканий) той или иной кнопки
 byte butcount = 0;
 
@@ -85,27 +106,27 @@ unsigned long klapanMs = 0;
 void setup() {
   Serial.begin(115200);
   dht.begin();
-pinMode(BUTTON1, INPUT_PULLUP);
-pinMode(BUTTON2, INPUT_PULLUP);
-pinMode(BUTTON3, INPUT_PULLUP);
-pinMode(BUTTON4, INPUT_PULLUP);
-pinMode(LAMP1, OUTPUT);
-pinMode(LAMP2, OUTPUT);
-pinMode(LAMP3, OUTPUT);
-pinMode(RGBWSTRIP, OUTPUT);
-pinMode(LEDLAMP1, OUTPUT);
-pinMode(LEDLAMP2, OUTPUT);
-pinMode(LEDLAMP3, OUTPUT);
-pinMode(LEDRGBWSTRIP, OUTPUT);
-pinMode(KLAPAN, OUTPUT);
-digitalWrite(KLAPAN , RELAY_OFF);
-makelamps(); // включим лампы и подсветку как надо
+  pinMode(BUTTON1, INPUT_PULLUP);
+  pinMode(BUTTON2, INPUT_PULLUP);
+  pinMode(BUTTON3, INPUT_PULLUP);
+  pinMode(BUTTON4, INPUT_PULLUP);
+  pinMode(LAMP1, OUTPUT);
+  pinMode(LAMP2, OUTPUT);
+  pinMode(LAMP3, OUTPUT);
+  pinMode(RGBWSTRIP, OUTPUT);
+  pinMode(LEDLAMP1, OUTPUT);
+  pinMode(LEDLAMP2, OUTPUT);
+  pinMode(LEDLAMP3, OUTPUT);
+  pinMode(LEDRGBWSTRIP, OUTPUT);
+  pinMode(KLAPAN, OUTPUT);
+  digitalWrite(KLAPAN , RELAY_OFF);
+  makelamps(); // включим лампы и подсветку как надо
 }//setup
 
 void loop() {
 
-ms = millis();
-autoButfunc(); // опрос кнопок и работа света
-autoKlapanfunc(); // работа клапана
+  ms = millis();
+  autoButfunc(); // опрос кнопок и работа света
+  autoKlapanfunc(); // работа клапана
 
 }//loop
